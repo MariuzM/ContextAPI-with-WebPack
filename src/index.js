3b3de62d155819c0ff0c4574cb1bb3c1b59fc9f1
Marius <marius@Mariuss-MacBook-Pro.local>
2019-08-22 15:10:25 +0100

    Initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ProductProvider } from './context/context';
+
+ReactDOM.render(
+  <ProductProvider>
+    <App />
+  </ProductProvider>,
+  document.getElementById('root')
+);
